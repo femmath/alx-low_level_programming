3d99e87d2e3454aed0c1fde26a193dc900c221a0
femmath <femmath11@gmail.com>
2023-03-16 13:03:31 -0700

    Program that prints exactly a line of text

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line using printf function
+ * Description: 'A C program that prints exactly a line of text'
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
