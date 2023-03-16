6aef4b1a0885c3b0ee76d49652ad50904a80a1cb
femmath <femmath11@gmail.com>
2023-03-16 13:52:57 -0700

    a program thet prints the size of the computer it is compiled

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - A C program that prints the size of various types on the computer it is compiled
+ * main - A C program that prints the size of compiled computer
  * Description: 'The program displays the sizes of the data types'
  * Return: 0 (Success)
  */
@@ -16,5 +16,5 @@ printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeoe(d));
 printf("size of a float int: %lu byte(s)\n", (unsigned long)sizeof(f));
-return(0);
+return (0);
 }
