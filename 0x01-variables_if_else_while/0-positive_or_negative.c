89f0d8e31a88716068a408bbde3a24d98195860f
femmath <femmath11@gmail.com>
2023-03-17 15:39:38 -0700

    positivenumbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,27 +1,32 @@
-/**
+/*
  * File: 0-positive_or_negative.c
  * Auth: Oluwafemi Olalere
  */
+
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
  * main - Prints a random number and states whether
  *        it is positive, negative, or zero.
-* Description: 'printing status of a numbee'
-* Return: Always 0 (Success)
+ *Description: a c program to identify positive, negative and zeros 
+ * Return: Always 0.
 */
 int main(void)
 {
 	int n;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n < 0)
 		printf("%d is negative\n", n);
 	else
 		printf("%d is zero\n", n);
+
 	return (0);
 }
+
