ac1d2df3ec1411c1fd25a1d9516272e4b3b33486
femmath <femmath11@gmail.com>
2023-03-21 16:36:26 -0700

    0-put_char

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
-/**
- * main - The program to print _puchar
- * Description: written by Olalere
- * Return: 0
- */
-int main(void);
-{
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
+#include "main.h"
+	/**
+	 * main - Thi is a program that prints char
+	 * Description: This code written by olalere
+	 * Return: 0
+	 */
 
-_putchar('r');
-_putchar('\n');
-return (0);
-}
+	int main(void)
+	{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+	}
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,13 +1,14 @@
 #include "main.h"
-#include <unistd.h>
-/**
- * _putchar - write the character c to stdout
- * @c: Th character to print
- * 
- * Return: On succes 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char)
-{
-	return (write(1, &c, 1));
-}
+	#include <unistd.h>
+	/**
+	 * _putchar - writes the character c to stdout
+	 * @c: The character to print
+	 *description: program to print character c
+	 * Return: On success 1.
+	 * On error, -1 is returned, and errno is set appropriately.
+	 */
+	int _putchar(char c)
+	{
+		return (write(1, &c, 1));
+	}
+
