7283c57027511fc2b58dae4fd1b443e355f3f333
femmath <femmath11@gmail.com>
2023-03-21 17:33:06 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+
+	/**
+	 * main - check the code.
+	 * Description: 'This is the main function for printing 10x alphabets'
+	 * Return: Always 0.
+	 */
+	int main(void)
+	{
+	    print_alphabet_x10();
+	    return (0);
+	}
+
