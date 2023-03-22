7b1953b4c20fe6cc44a9398e766f54ccf0fe53c0
femmath <femmath11@gmail.com>
2023-03-21 17:07:35 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,12 +1,14 @@
 #include "main.h"
 	#include <unistd.h>
 	/**
-	 * _putchar - writes the character c to stdout
+	 * main - Main Entry
+	 * _putchar - 'writes the character c to stdout'
 	 * @c: The character to print
-	 *description: program to print character c
+	 * Description: 'This is a Program to to putchar c'
 	 * Return: On success 1.
 	 * On error, -1 is returned, and errno is set appropriately.
 	 */
+
 	int _putchar(char c)
 	{
 		return (write(1, &c, 1));
